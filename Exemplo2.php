e7919afd35e7dc28ff7458fa40b8d787fde8db21
xubirubi <jonas@gmail.com>
2023-11-10 18:00:38 -0300

    Adicionei o exemplo 2.

---
+++ Exemplo2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 2</title>
+</head>
+<body>
+    <h2>Exemplo 2</h2>
+    <?php
+$x = 5;
+$y = 4;
+echo "x=".$x;
+echo "<br> y=".$y;
+echo "<br> x+y=".$x + $y;
+?>
+</body>
+</html>
